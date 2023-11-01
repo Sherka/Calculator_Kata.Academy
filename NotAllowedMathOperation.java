d2ee9d237deb0594d82c720a36c31bca0d8b550f
Mikhail <biker_93@mail.ru>
2023-11-01 13:24:01 +0400

    Refactor for Git repo

--- NotAllowedMathOperation.java
+++
@@ -1,3 +0,0 @@
-public class NotAllowedMathOperation extends Exception {
-    public NotAllowedMathOperation() { super(); }
-}
--- NotOnlyRomanNums.java
+++
@@ -1,3 +0,0 @@
-public class NotOnlyRomanNums extends Exception {
-    public NotOnlyRomanNums() { super(); }
-}
